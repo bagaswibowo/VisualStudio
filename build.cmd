29650c26b22a4c411e3664a698bd0b6541107536
bagaswibowo <36736385+bagaswibowo@users.noreply.github.com>
2018-04-05 12:08:13 +0700

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 @if "%1" == "" echo Please specify Debug or Release && EXIT /B
-powershell -ExecutionPolicy Unrestricted scripts\build.ps1 -Package:$true -Config:%1
+powershell -ExecutionPolicy Unrestricted scripts\build.ps1 -Package:$true -Config:%0
